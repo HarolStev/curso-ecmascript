bdf38587ee909ae72d3ad272d2f0a08ed949edd6
Harol Gonzalez <harolgonza@outlook.es>
2022-12-11 23:28:55 -0500

    Cambios finales

--- src/es10/02-try-catch.js
+++ src/es10/02-try-catch.js
@@ -11,3 +11,17 @@ try {
 } catch {
     console.log('No esta definido la función');
 }
+
+/*
+try {
+  // Manejar el código
+} catch (err) {
+  // Se utiliza el parámetro `err`
+}
+
+try {
+  // Manejar el código
+} catch {
+  // Manejar el error sin el parámetro.
+}
+*/
